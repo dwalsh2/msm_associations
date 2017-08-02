ddb4f1a3cbe87750d4a1a17bc09d445e28320ca7
Dan Walsh <dwalsh2@chicagobooth.edu>
2017-08-02 21:49:14 +0000

    fixed director id

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -13,7 +13,7 @@
 
 class Actor < ApplicationRecord
 #  - name: must be present; must be unique in combination with dob
-    validates :director_id, :presence => true, :uniqueness => { :scope => :dob }
+    validates :name, :presence => true, :uniqueness => { :scope => :dob }
 #  - dob: no rules
 #  - bio: no rules
 #  - image_url: no rules
