3633305cce606610ba163f535b164bc21d01ee97
Dan Walsh <dwalsh2@chicagobooth.edu>
2017-08-02 22:32:02 +0000

    actor fixed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -20,4 +20,6 @@ class Actor < ApplicationRecord
 
     has_many(:characters, :class_name => "Character", :foreign_key => "actor_id")
     
+    has_many(:movies, :through => :characters)
+    
 end
